adeb32b22a19860568c3d78cb5bccbf7c16b4264
Hellen <hrpalmaen@gmail.com>
2019-11-14 17:29:07 -0500

    Se inicializa formulario de creación de usuarios

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
 
-export class User extends Component {
+import TextField from '@material-ui/core/TextField';
+
+export class Create extends Component {
   constructor() {
     super();
   }
-
+  
   render() {
     return (
       <div>
         <h3>Usuarios</h3>
+        <TextField
+          id="standard-basic"
+          // className={classes.textField}
+          label="Nombres"
+          margin="normal"
+        />
+        <TextField
+          id="standard-basic"
+          // className={classes.textField}
+          label="Apellidos"
+          margin="normal"
+        />
+        <TextField
+          id="standard-basic"
+          // className={classes.textField}
+          label="Documento"
+          margin="normal"
+        />
+        <TextField
+          required
+          id="standard-basic"
+          // className={classes.textField}
+          label="Usuario"
+          margin="normal"
+        />
+        <TextField
+          required
+          id="standard-basic"
+          // className={classes.textField}
+          label="Tipo usuario"
+          margin="normal"
+        />
       </div>
     );
   }
