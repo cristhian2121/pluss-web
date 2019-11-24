1d1d1a5bec332ce904d23b33a8490ce10926b513
Hellen Palma <hrpalmaen@gmail.com>
2019-11-24 17:45:46 -0500

    formulario de creación funcional

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -10,20 +10,18 @@ import Button from '@material-ui/core/Button';
 export class Create extends Component {
   state = {
     dataGroups: [],
-    data: {
-      group: null,
-      name: null
-    }
   }
-
-  constructor(props) {
-    super(props);
-  }  
-
+  data = {
+    code: null,
+    name: null,
+    document: null,
+    email: null,
+    phone: null,
+    group: null
+  }
   componentDidMount() {
     this.getGroups()
   }
-
   getGroups = async () => {
     try {
       let response = await fetch('http://rickandmortyapi.com/api/character/')
@@ -36,16 +34,26 @@ export class Create extends Component {
       console.log('error', error)
     }    
   }
-
-  save() {
-    let name = this.state.data.name
-    console.log('llego', name)
-  }
-
-  handleChange(event) {
-    this.setState({
-      data: event.target.value
-    })
+  handleChange = e => {
+    console.log('este es el nombre', e.target.name);
+    switch (e.target.name){
+      case "code":
+        this.data.code = e.target.value
+      case "name":
+        this.data.name = e.target.value
+      case "document":
+        this.data.document = e.target.value
+      case "email":
+        this.data.email = e.target.value
+      case "phone":
+        this.data.phone = e.target.value
+      case "group":
+        this.data.group = e.target.value
+    }
+    console.log('llego', this.data.name)
+  };  
+  save = () => {
+    console.log('llego al save', this.data)
   }
 
   render() {
@@ -57,55 +65,59 @@ export class Create extends Component {
         <form>
           <TextField
             required
-            id="code"
+            onChange={this.handleChange}
+            name="code"
             label="Código"
             margin="normal"
             />
           <TextField
             required
-            id="name"
+            onChange={this.handleChange}
+            name="name"
             label="Nombre"
+            value={this.data.name}
             margin="normal"
-            value={this.state.data.name}
             />
           <TextField
-            id="document"
+            onChange={this.handleChange}
+            name="document"
             label="Documento"
             margin="normal"
             />
           <TextField
             required
-            id="email"
+            onChange={this.handleChange}
+            name="email"
             label="Correo electrónico"
             margin="normal"
             />
           <TextField
             required
-            id="phone"
+            onChange={this.handleChange}
+            name="phone"
             label="Teléfono"
             margin="normal"
             />
           <FormControl margin="normal">
-            <InputLabel id="name_type_user">Tipo usuario</InputLabel>
+            <InputLabel id="group">Tipo usuario</InputLabel>
             <Select
-              labelId="type_user"
-              id="type_user"
-              value={this.state.group}
+              labelId="group"
+              name="group"
               onChange={this.handleChange}
+              // onInput={this.handleChange}
             >              
               {this.state.dataGroups.map(groups => (
-                <MenuItem key={groups.id} value={groups.id}>{groups.name}</MenuItem>
+                <MenuItem value={groups.id}>{groups.name}</MenuItem>
               ))}              
-            </Select>
+            </Select> 
           </FormControl>
+          <br/><br/>
+          <div class="text-center">
+            <Button color="primary" onClick={this.save}>
+              Crear Usuario
+            </Button>
+          </div>
         </form>
-
-        <div class="text-center">
-          <Button variant="contained" color="primary" onClick={this.save}>
-            Crear Usuario
-          </Button>
-        </div>
-
       </div>
     );
   }
