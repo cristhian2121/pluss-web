6e874c04c80fd9842f904b43c18e0cda20891e97
cristhian2121 <ccdelgadop@gmail.com>
2019-12-15 17:26:24 -0500

    Separar login y pdf del menu usando a travez de rutas

--- src/components/common/pdf/index.js
+++ src/components/common/pdf/index.js
@@ -48,8 +48,8 @@ export const GeneratePDFHook = (props) => {
             setUnitsCost(units);
         }
     }
-    const $navBar = document.querySelector('#nav-var-pluss')
-    $navBar.style.visibility = 'collapse'
+    // const $navBar = document.querySelector('#nav-var-pluss')
+    // $navBar.style.visibility = 'collapse'
     console.log('props: ', props);
     return (
         <div>
