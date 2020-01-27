957df9a70dae8013db32c83d282abfbbbfd06122
Hellen <hrpalmaen@gmail.com>
2020-01-27 17:05:10 -0500

    modify: Peticiones en módulo clientes

--- src/components/clients/client.js
+++ src/components/clients/client.js
@@ -1,23 +1,30 @@
 import React from "react"
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
+
 import { CreateClient } from './createClient'
 import { ClientList } from './clientList'
 import config from '../../config'
-export class Clients extends React.Component {
 
+export class Clients extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      clients: []
+      clients: [],
+      updateClient: [],
+      alert: {
+        open: false,
+        message: '',
+        type: '',
+      }
     }
-
-    this.insertClient = this.insertClient.bind(this)
+    // this.insertClient = this.insertClient.bind(this)
     this.clientDelete = this.clientDelete.bind(this)
   }
 
   componentDidMount() {
     this.getClient()
-
   }
 
   getClient() {
@@ -36,60 +43,72 @@ export class Clients extends React.Component {
       })
   }
 
-  insertClient = (client) => {
+  saveClient = (client) => {
     console.log('client: ', client);
-    this.setState({
-      clients: [...this.state.clients, client]
+    fetch(`${config.api_url}/client/`,
+      {
+      method: 'POST',
+      body: JSON.stringify(client),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .catch(error => console.log('Error: ', error))
+    .then(response => {
+      this.setState({
+        clients: [...this.state.clients, client],
+        alert: {
+          open: true,
+          message: 'El cliente se agrego.',
+          type:'success'
+        }
+      })
+      this.clearForm()
     })
 
   }
+
+  // insertClient = (client) => {
+  //   this.setState({
+  //     clients: [...this.state.clients, client],
+  //     alert: {
+  //       open: true,
+  //       message: 'El usuario se agrego',
+  //       type:'success'
+  //     }
+  //   })
+  // }
+
 
   clientDelete = (client) => {
-    console.log('clientclientclient: ', client);
-    const clients = this.state.clients.filter(item => item.id != client.id)
-    console.log('clients: ', clients);
-    this.setState({
-      clients: clients
+    fetch(`${config.api_url}/client/${client.id}` , {method: 'DELETE'})
+    .then(response => {        
+      if (response.status === 204) {
+        let clients = this.state.clients.filter(item => item.id != client.id)
+        this.setState({ 
+          clients: clients,
+          alert: {
+            open: true,
+            message: 'El cliente se elimino.',
+            type:'success'
+          }
+        })
+      }
     })
-    // for (let [item, index] of this.state.clients.entries()) {
-    //   console.log('item: ', inde);
-    //   if (item.id === client.id) {
-    //     const clientsAux = [...this.state.clients]
-    //     console.log('clientsAux: ', clientsAux[0]);
-    //     const newClient = clientsAux.splice(index, 1)
-    //     console.log('newClient: ', newClient);
-        
-    //     console.log('clients: ', this.state.clients);
-    //   }
-    // }
-    // fetch(`${config.api_url}/client/${client.id}` , {method: 'DELETE'})
-    // .then(response => {        
-    //   if (response.status === 204) {
-    //     this.props.clientDelet(client)
-    //     // for (let item of this.props.clientList ) {
-    //     //   if (item.id === client.id) {
-    //     //     this.props.clientList = this.props.clientList.splice(item, 1)
-    //     //     return
-    //     //   }
-    //     // }
-    //   }
-    // })
-    // .then(res => { console.log('reseeeeeeeeeeeeeeee: ', res) })
-    // .catch(e => { console.log('Erroraaaaaa: ', e) })
-
+    .then(res => { console.log('res: ', res) })
+    .catch(e => { console.log('Error: ', e) })
   }
-  // clientDelete(client) {
-  //   console.log('client: ', this.state.clients);
-  //   for (let item of this.state.clients ) {
-  //     console.log('item: ', item);
-  //     if (item.id === client.id) {
-  //       this.setState({
-  //         clients: this.clients.splice(item, 1)
-  //       })
-  //       return
-  //     }
-  //   }
-  // }
+
+  clearForm () {
+    document.getElementById("clientForm").reset()
+  }
+
+  selectUpdate = (client) => {
+    console.log('client: ', client);
+    this.setState({
+      updateClient: client
+    })
+  }
 
   render() {
 
@@ -99,9 +118,22 @@ export class Clients extends React.Component {
           Clientes
         </div>
         <br />
-        <CreateClient addClientToList={this.insertClient} />
+        <CreateClient saveClient={this.saveClient} clientUpdate={this.state.updateClient} />
         <br /><br />
-        <ClientList selectDelet={this.clientDelete} clientList={this.state.clients} />
+        <ClientList selectDelete={this.clientDelete} selectUpdate={this.selectUpdate} clientList={this.state.clients} />
+
+        <Snackbar
+          open={this.state.alert.open}
+          autoHideDuration={4000}
+          onClose={() => 
+            this.setState({alert: {open: false}})
+          }
+          anchorOrigin= {{ 
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}>
+          <Alert severity={this.state.alert.type}>{this.state.alert.message}</Alert>
+        </Snackbar>
       </>
     )
   }
--- src/components/clients/createClient.js
+++ src/components/clients/createClient.js
@@ -7,11 +7,30 @@ import conf from '../../config'
 export class CreateClient extends React.Component {
 
   constructor(props) {
+    console.log('props: ', props);
     super(props)
-    this.saveClient = this.saveClient.bind(this)
+    this.state = {
+      dataEdit: false
+    }
+    // this.client = this.client.bind(this)
   }
 
-  saveClient(event) {
+  componentWillReceiveProps = (nextProps)=>{
+    console.log('nextProps: ', nextProps);
+    this.setState({
+      dataEdit: true,
+      name: nextProps.clientUpdate ? nextProps.clientUpdate.name : null,
+      nit: nextProps.clientUpdate ? nextProps.clientUpdate.nit : null,
+      phone: nextProps.clientUpdate ? nextProps.clientUpdate.phone : null,
+      agent: nextProps.clientUpdate.agent ? nextProps.clientUpdate.agent.name : null,
+      city: nextProps.clientUpdate ? nextProps.clientUpdate.city : null,
+      address: nextProps.clientUpdate ? nextProps.clientUpdate.address : null,
+      email: nextProps.clientUpdate ? nextProps.clientUpdate.email : null,
+      phone_two: nextProps.clientUpdate ? nextProps.clientUpdate.phone_two : null,
+    })
+  }
+
+  addClient = (event) => {
     event.preventDefault()
     let name = document.getElementById('name').value
     let nit = document.getElementById('nit').value
@@ -21,7 +40,9 @@ export class CreateClient extends React.Component {
     let address = document.getElementById('address').value
     let email = document.getElementById('email').value
     let phone_two = document.getElementById('phone_two').value
-    const validate = this.validator(name, nit, phone, agent, city, address)
+    // let data = this.generateData()
+    // console.log('aa: ', data);
+    const validate = this.validator(name, nit)
     if(validate){
       const client = {
         name: name,
@@ -36,7 +57,8 @@ export class CreateClient extends React.Component {
         phone_two: phone_two,
       }
       console.log("ok", client)
-      this.addClient(client)
+      this.props.saveClient(client)
+      // this.addClient(client)
     }else{
       console.log('faltan datos')
     }
@@ -63,25 +85,49 @@ export class CreateClient extends React.Component {
     }
   }
 
-  addClient(client){
-    fetch(`${conf.api_url}/client/`,{
-      method: 'POST',
-      body: JSON.stringify(client),
-      headers:{
-        'Content-Type': 'application/json'
-      }
-    }).then(res => res.json())
-    .catch(error => console.log('Error: ', error))
-    .then(response => {
-      console.log('Success: ', response)
-      this.props.addClientToList(response)
-      this.clearForm()
-    })
-  }
-
   clearForm = () => {
     this.client = {}
     document.getElementById("clientForm").reset()
+    this.setState({
+      name: null,
+      nit: null,
+      city: null,
+      address: null,
+      agent: null,
+      email: null,
+      phone: null,
+      phone_two: null
+    })
+  };
+
+  handleChange = e => {
+    this.render()
+    switch (e.target.name) {
+      case "name":
+        this.setState({ name: e.target.value })
+        break
+      case "nit":
+        this.setState({ nit: e.target.value })
+        break
+      case "city":
+        this.setState({ city: e.target.value })
+        break
+      case "address":
+        this.setState({ address: e.target.value })
+        break
+      case "agent":
+        this.setState({ agent: e.target.value })
+        break
+      case "email":
+        this.setState({ email: e.target.value })
+        break
+      case "phone":
+        this.setState({ phone: e.target.value })
+        break
+      case "phone_two":
+        this.setState({ phone_two: e.target.value })
+        break
+    }
   };
 
   render() {
@@ -89,27 +135,27 @@ export class CreateClient extends React.Component {
       <>
         <div className="sub-title">
           <Button onClick={this.showForm}>
-            Crear Cliente
+            {this.state.dataEdit ? 'Editar cliente' : 'Crear Cliente' }
           </Button>
         </div>
         <form noValidate autoComplete="off" id="clientForm">
-          <TextField id="name" label="Nombre empresa" />
-          <TextField id="nit" label="Nit" />
-          <TextField id="city" label="Ciudad" />
-          <TextField id="address" label="Dirección" />
-          <TextField id="agent" label="Nombre representante" />
-          <TextField id="email" label="Correo electrónico" />
-          <TextField id="phone" label="Teléfono" />
-          <TextField id="phone_two" label="Teléfono alternativo" />
+          <TextField id="name" name="name" label="Nombre empresa" value={this.state.name} onChange={this.handleChange}/>
+          <TextField id="nit" name="nit" label="Nit" value={this.state.nit} onChange={this.handleChange}/>
+          <TextField id="city" name="city" label="Ciudad" value={this.state.city} onChange={this.handleChange}/>
+          <TextField id="address" name="address" label="Dirección" value={this.state.address} onChange={this.handleChange}/>
+          <TextField id="agent" name="agent" label="Nombre representante" value={this.state.agent} onChange={this.handleChange}/>
+          <TextField id="email" name="email" label="Correo electrónico" value={this.state.email} onChange={this.handleChange}/>
+          <TextField id="phone" name="phone" label="Teléfono" value={this.state.phone} onChange={this.handleChange}/>
+          <TextField id="phone_two" name="phone_two" label="Teléfono alternativo" value={this.state.phone_two} onChange={this.handleChange}/>
           {/* <Button variant="contained" color="primary" type="submit" onClick={this.saveClient}>
             Guardar
           </Button> */}
           <div className="text-center">
             <br/>
-            <Button variant="contained" color="secondary" onClick={this.clearForm}>
+            <Button variant="contained" onClick={this.clearForm}>
               Limpiar
             </Button>
-            <Button variant="contained" color="primary" onClick={this.saveClient}>
+            <Button variant="contained" color="secondary" onClick={this.addClient}>
               Guardar
             </Button>
           </div>
