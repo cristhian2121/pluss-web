cbf218913e45a2224ec9741fd0f2793a1d0b5b06
maestro_roshi <ccdelgadop@gmail.com>
2021-06-01 15:56:56 -0500

    Add git workflow and codeploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    // api_url: 'http://localhost:8933/api',
+    api_url: 'http://13.58.35.221:8933/api',
     // api_products: 'https://mppromocionales.com/ws_products.php?token=04822d0d1e40429463a48c40bff430bf7874be23e304f44e3661bbc44e9e6b83'
-    api_url: 'http://localhost:8933/api',
+    // api_url: 'http://localhost:8933/api',
     api_products: 'https://www.mppromocionales.com/images/grandes/',
     EXTENSION_IMAGE: 'jpg',
     PROVEEDORES: {
