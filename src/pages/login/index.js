dee88ca936476c0006970da274f4c473867f5c2d
maestro_roshi <ccdelgadop@gmail.com>
2021-06-26 12:06:15 -0500

    Improve login style

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,41 +1,61 @@
-import React, { useState } from "react";
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { SnackbarProvider, useSnackbar } from 'notistack';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import IconButton from '@material-ui/core/IconButton';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import React, { useState, useEffect } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { SnackbarProvider, useSnackbar } from "notistack";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import conf from '../../config';
-import Copyright from '../../components/common/copyright'
-import { Redirect } from 'react-router-dom'
-import Logo from '../../static/Logo.png'
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import conf from "../../config";
+import Copyright from "../../components/common/copyright";
+import { Redirect } from "react-router-dom";
+import Logo from "../../static/Logo.png";
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+const useStyles = makeStyles((theme) => {
+  const getPropertiesRoot = () => {
+    const $root = document.querySelector(":root");
+    const color = getComputedStyle($root).getPropertyValue("--primary-color");
+    const textColor = getComputedStyle($root).getPropertyValue(
+      "--bg-color-ligth"
+    );
+    return {
+      backGroundColor: color,
+      textColor: textColor,
+    };
+  };
+
+  const { backGroundColor, textColor } = getPropertiesRoot();
+
+  return {
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(2, 0),
+      padding: theme.spacing(2, 1),
+      background: backGroundColor,
+      color: textColor,
+      fontSize: '1rem'
+    },
+  };
+});
 
 export default function Login() {
   setTimeout(() => {
@@ -53,49 +73,59 @@ export default function Login() {
 function IntegrationNotistack() {
   const classes = useStyles();
   const { enqueueSnackbar } = useSnackbar();
-  const [loginRoot, setLoginRoot] = useState(false)
-  const [showPassword, setShowPassword] = useState(false)
+  const [loginRoot, setLoginRoot] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const login = (event) => {
-    event.preventDefault()
-    let data = generateData()
-    console.log('data: ', data);
+    event.preventDefault();
+    let data = generateData();
     fetch(`${conf.api_url}/login/`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     })
       .then(async function (response) {
-        console.log('MELO');
-        let resp = await response.json()
-        console.log('resp login: ', resp);
-        if (resp['username']) { enqueueSnackbar(resp['username'], { variant: 'error' }) }
-        if (resp['password']) { enqueueSnackbar(resp['password'], { variant: 'error' }) }
-        if (response.status == 401) { enqueueSnackbar(resp['detail'], { variant: 'error' }) }
-        if (response.status == 200) { setPermissions(resp) }
+        let resp = await response.json();
+        if (resp["username"]) {
+          enqueueSnackbar(resp["username"], { variant: "error" });
+        }
+        if (resp["password"]) {
+          enqueueSnackbar(resp["password"], { variant: "error" });
+        }
+        if (response.status == 401) {
+          enqueueSnackbar(resp["detail"], { variant: "error" });
+        }
+        if (response.status == 200) {
+          setPermissions(resp);
+        }
       })
       .catch(function (error) {
-        console.log('error: ', error);
-        enqueueSnackbar('Se generó un error en la autenticación.', { variant: 'error' });
+        console.log("error: ", error);
+        enqueueSnackbar("Se generó un error en la autenticación.", {
+          variant: "error",
+        });
       });
-  }
+  };
 
   const generateData = () => {
-    const elements = document.getElementById('loginForm').elements;
+    const elements = document.getElementById("loginForm").elements;
     let data = {};
-    data.username = elements.username.value.trim()
-    data.password = elements.password.value.trim()
-    return data
-  }
+    data.username = elements.username.value.trim();
+    data.password = elements.password.value.trim();
+    return data;
+  };
 
   const setPermissions = (user) => {
-    console.log('user: ', user);
-    localStorage.name = user.name
-    localStorage.ldap = user.permission
-    setLoginRoot(true)
-  }
+    localStorage.name = user.name;
+    localStorage.ldap = user.permission;
+    setLoginRoot(true);
+  };
+
+  useEffect(() => {
+    // if($root) $root.style
+  });
 
   return (
     <Container component="main" maxWidth="xs">
@@ -122,7 +152,7 @@ function IntegrationNotistack() {
             autoComplete="username"
             autoFocus
             onKeyPress={(event) => {
-              (event.key === "Enter") && login(event)
+              event.key === "Enter" && login(event);
             }}
           />
           <TextField
@@ -138,17 +168,21 @@ function IntegrationNotistack() {
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
-                  <IconButton 
+                  <IconButton
                     aria-label="toggle password visibility"
                     onClick={() => setShowPassword(!showPassword)}
                   >
-                    {showPassword ? <Visibility className="icon-size" /> : <VisibilityOff className="icon-size" />}
+                    {showPassword ? (
+                      <Visibility className="icon-size" />
+                    ) : (
+                      <VisibilityOff className="icon-size" />
+                    )}
                   </IconButton>
                 </InputAdornment>
-              )
+              ),
             }}
             onKeyPress={(event) => {
-              (event.key === "Enter") && login(event)
+              event.key === "Enter" && login(event);
             }}
           />
           {/* <FormControlLabel
@@ -159,7 +193,6 @@ function IntegrationNotistack() {
             type="submit"
             fullWidth
             variant="contained"
-            color="primary"
             className={classes.submit}
             onClick={login}
           >
@@ -170,7 +203,7 @@ function IntegrationNotistack() {
       <Box mt={8}>
         <Copyright />
       </Box>
-      {loginRoot ? <Redirect to='/cotizaciones' /> : ''}
+      {loginRoot ? <Redirect to="/cotizaciones" /> : ""}
     </Container>
   );
 }
